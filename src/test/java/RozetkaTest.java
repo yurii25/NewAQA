c54ffb92d25a8508a18739d5ee46facbd3e9ec4d
Baitsar Yurii <baitsar@testfort.com>
2023-06-14 13:48:48 +0300

    init

--- src/test/java/RozetkaTest.java
+++ src/test/java/RozetkaTest.java
@@ -3,27 +3,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.$x;
-
-/*
-"- Open http://rozetka.com.ua home page
-- Search for ‘iPhone’
-- Verify that at least 5 result links are displayed and that each link text contains 'iPhone'
-- Verify the 3rd product has a price, save the price
-- Click on the 3rd product and verify that the stored price and price on product page is equal
-
-- Open http://rozetka.com.ua home page
-- Click on the user icon in the top right corner
-- Verify that the login modal is displayed
-- Close the login modal, verify it’s closed
-
-- Open http://rozetka.com.ua home page
-- Search for ‘dell’
-- Verify that at least 10 result links are displayed
-- Save the name and the price of 5th product, add it to the basket
-- Verify the basket modal is opened and the price and name are correct
-
-Create PR and assing toleksyn as reviewer."
- */
 public class RozetkaTest {
 
     @Test
